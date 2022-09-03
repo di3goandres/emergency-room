ce6a63ed1319d962ab0e6b3a29d29d3eec905060
DiegoAndresMontealegre <diego.montealegre@mercadolibre.com.co>
2022-09-02 21:42:43 -0500

    port test

--- src/main/java/com/arquitectura/emergency/room/EmergencyRoomApplication.java
+++ src/main/java/com/arquitectura/emergency/room/EmergencyRoomApplication.java
@@ -4,8 +4,10 @@ import com.arquitectura.emergency.room.config.SpringConfig;
 import com.arquitectura.emergency.room.util.ScopeUtils;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class EmergencyRoomApplication {
 
     public static void main(String[] args) {
